43b893e1668ed8fe8f4d32bd0c16f3fb9a1f085f
Terrance Hui <terrancehui@Terrances-MacBook-Pro.local>
2017-08-15 16:48:01 +0800

    updated after ios cert added

--- ios/manager/main.m
+++ ios/manager/main.m
@@ -1,6 +1,6 @@
 /**
  * Copyright (c) 2015-present, Facebook, Inc.
- * All rights reserved.
+ * All rights reserved. 2
  *
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree. An additional grant
